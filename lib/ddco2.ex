f36ed5bade20ff5ff11b1ddf7618f1fc361a8906
Adrian Irving-Beer <wisq@wisq.net>
2019-02-12 00:12:45 -0500

    `mix new ddco2`

---
+++ lib/ddco2.ex
@@ -0,0 +1,18 @@
+defmodule Ddco2 do
+  @moduledoc """
+  Documentation for Ddco2.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ddco2.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ddco2_test.exs
@@ -0,0 +1,8 @@
+defmodule Ddco2Test do
+  use ExUnit.Case
+  doctest Ddco2
+
+  test "greets the world" do
+    assert Ddco2.hello() == :world
+  end
+end
