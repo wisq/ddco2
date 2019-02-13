7fad32560e7149d746b2a5fe9560701260c717cb
Adrian Irving-Beer <wisq@wisq.net>
2019-02-12 22:03:56 -0500

    Use released version of ex_co2_mini.

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule DDCO2.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:ex_co2_mini, git: "https://github.com/wisq/ex_co2_mini.git"},
+      {:ex_co2_mini, "~> 0.1.3"},
       {:statix, "~> 1.1.0"},
       {:distillery, "~> 2.0", runtime: false}
     ]
